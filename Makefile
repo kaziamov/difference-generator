fb9d173434fe1b63afb2ba9fa05491edf6fa93af
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-31 18:58:53 +0300

    Update cli-script

--- Makefile
+++ Makefile
@@ -2,5 +2,14 @@
 
 gendiff:
 	poetry run gendiff
-
+lint:
+	poetry run flake8 brain_games
+install:
+	poetry install
+build:
+	poetry build
+publish:
+	poetry publish --dry-run
+package-install:
+	python3 -m pip install --user dist/*.whl --force
 
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,4 @@
-#!
+#!/usr/bin/env python
 
 # Import build-in modules
 import argparse
@@ -6,16 +6,17 @@ import argparse
 
 def main():
     """Сreate documentation for programm"""
-    parser = argparse.ArgumentParser(description='Program for compare two files and print difference.')
+    parser = argparse.ArgumentParser(prog='gendiff', description='Program for compare two files and print difference.')
     parser.add_argument('first_file',
-                        metavar='first_file',
                         help='First file to compare')
     parser.add_argument('second_file',
-                        metavar='second_file',
                         help='Second file to compare')
+    parser.add_argument('-f', '--format',
+                        metavar='FORMAT',
+                        help='Get choice for output file format')
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    # print(args.accumulate(args.integers))
 
 
 if __name__ == '__main__':
