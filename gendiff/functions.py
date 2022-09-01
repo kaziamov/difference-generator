ec73687075d8f976e921c81fb4b32b9e6eaea81f
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-09-01 13:21:35 +0300

    Update funcs and add tests

--- gendiff/functions.py
+++ gendiff/functions.py
@@ -4,11 +4,25 @@
 import json
 
 
-def open_file():
+def open_file(path_to_file, format='json'):
+    with open(path_to_file, 'r') as file_to_dict:
+        dict = json.loads(file_to_dict)
+        return dict
+
+def open_two_files(files):
+    # return map(open_file, files)
+    return [open_file(file) for file in files]
+
+def create_set(iterable_for_set):
+    return set(iterable_for_set)
+
+def get_union(list_of_dict):
     pass
 
-def create_magic():
+def get_intersection_of_many(list_of_dict):
     pass
+
+
 
 def generate_diff():
     pass
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,6 +3,12 @@ from gendiff import functions
 file1 = 'file1.json'
 file2 = 'file2.json'
 
-def test_generate_diff():
-    assert functions.generate_diff(file1, file2) == result
+def test_create_set():
+    assert functions.create_set([1, 2, 3, 4]) == {1, 2, 3, 4}
+    assert functions.create_set([1, 2, 3, 3, 2, 4]) == {1, 2, 3, 4}
+    assert functions.create_set(['a', 'b']) == {'a', 'b'}
+    assert functions.create_set({'a': 1, 'b': 2, 'c': 3}) == {'a', 'b', 'c'}
+
+# def test_generate_diff():
+#     assert functions.generate_diff(file1, file2) == result
 
