831f8006e22c733f0f1d12078238493a165e2beb
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-09-01 14:18:57 +0300

    Add new functions and tests

--- gendiff/functions.py
+++ gendiff/functions.py
@@ -5,27 +5,35 @@ import json
 
 
 def open_file(path_to_file, format='json'):
+    """
+    Open file in choicen format.
+    By default as JSON
+    """
     with open(path_to_file, 'r') as file_to_dict:
         dict = json.loads(file_to_dict)
         return dict
 
-def open_two_files(files):
-    # return map(open_file, files)
-    return [open_file(file) for file in files]
 
 def create_set(iterable_for_set):
     return set(iterable_for_set)
 
-def get_union(list_of_dict):
-    pass
 
-def get_intersection_of_many(list_of_dict):
-    pass
+def get_union(set1, set2):
+    return set1 | set2
 
 
+def get_intersection_of_many(set1, set2):
+    return set1 & set2
 
-def generate_diff():
-    pass
+
+def generate_diff(file1, file2, format='json'):
+    """
+    Conveyor for generate diff
+    for two files.
+    """
+    # return map(open_file, files)
+    [open_file(file) for file in files]
+
 
 def print_diff():
     pass
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,5 @@
 from gendiff import functions
 
-file1 = 'file1.json'
-file2 = 'file2.json'
 
 def test_create_set():
     assert functions.create_set([1, 2, 3, 4]) == {1, 2, 3, 4}
@@ -9,6 +7,32 @@ def test_create_set():
     assert functions.create_set(['a', 'b']) == {'a', 'b'}
     assert functions.create_set({'a': 1, 'b': 2, 'c': 3}) == {'a', 'b', 'c'}
 
+
+def test_get_union():
+    set1 = {'Paris', 'London'}
+    set2 = {'Moscow', 'Paris'}
+    result = {'London', 'Moscow', 'Paris'}
+    assert functions.get_union(set1, set2) == result
+
+
+def test_get_intersection_of_many():
+    set1 = {'Paris', 'London'}
+    set2 = {'Moscow', 'Paris'}
+    result = {'Paris'}
+    assert functions.get_intersection_of_many(set1, set2) == result
+
+
+
 # def test_generate_diff():
+#     file1 = 'file1.json'
+#     file2 = 'file2.json'
+#     result = '''{
+#   - follow: false
+#     host: hexlet.io
+#   - proxy: 123.234.53.22
+#   - timeout: 50
+#   + timeout: 20
+#   + verbose: true
+# }'''
 #     assert functions.generate_diff(file1, file2) == result
 
