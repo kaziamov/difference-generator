455feba5c17d62b9ae7777a2e34a9b3d744dbf1e
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-09-05 00:37:57 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,24 +2,25 @@
 
 # Import build-in modules
 import argparse
-from gendiff import generate_diff
+from functions import generate_diff
 
 
 def main():
-    """Сreate documentation for programm"""
+    """Сreate documentation for program"""
     parser = argparse.ArgumentParser(prog='gendiff', description='Program for compare two files and print difference.')
     parser.add_argument('first_file',
                         help='First file to compare')
     parser.add_argument('second_file',
                         help='Second file to compare')
-    # parser.add_argument('-f', '--format',
-    #                     metavar='FORMAT',
-    #                     help='Get choice for output file format')
+    parser.add_argument('-f', '--format',
+                        metavar='FORMAT',
+                        help='Get choice for output file format')
 
     args = parser.parse_args()
     # print(args.accumulate(args.integers))
     diff = generate_diff(args.first_file, args.second_file)
     print(diff)
+
 
 if __name__ == '__main__':
     main()
