5c112450c71adfe043255e7b2b2812ac5fc8eeb2
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-09-28 16:46:57 +0300

    Fix linter errors

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,7 +9,6 @@ import json
 # Import local modules
 
 
-
 def main():
     """Сreate documentation for program"""
     parser = argparse.ArgumentParser(prog='gendiff', description='Program for compare two files and print difference.')
@@ -21,14 +20,14 @@ def main():
                         metavar='FORMAT',
                         help='Get choice for output file format')
 
-    args = parser.parse_args()
-    functions.generate_diff(args.first_file, args.second_file)
+    # args = parser.parse_args()
+    # functions.generate_diff(args.first_file, args.second_file)
 
 
 def read_and_parse(path_to_file):
     with open(path_to_file, 'r') as file:
         return json.load(file)
-    
+
 
 def open_two_files(path_to_file1, path_to_file2):
     dict1 = read_and_parse(path_to_file1)
@@ -39,12 +38,12 @@ def open_two_files(path_to_file1, path_to_file2):
 def compare_files(two_dictionaries_in_tuple):
     dict1, dict2 = two_dictionaries_in_tuple
     set1, set2 = set(dict1), set(dict2)
-    
-    differences = []
+
+    # differences = []
     for key in sorted(set1 | set2):
         if dict1[key] == dict2[key]:
             pass
-    
+
 
 if __name__ == '__main__':
     main()
