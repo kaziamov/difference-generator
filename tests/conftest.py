c2eac5cbcb8e36e6e8b3f1349fa59be2d1bec6bc
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2023-01-13 00:16:35 +0300

    Add tests for plain formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,25 +11,33 @@ tree_dir = fixtures_path('tree/')
 expected_dir = fixtures_path('expected/')
 
 
-flat_expected = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+with open(expected_dir('flat_expected.txt'), 'r', encoding='utf-8') as file:
+    flat_expected = file.read()
 
+with open(expected_dir('flat_plain_expected.txt'), 'r', encoding='utf-8') as file:
+    flat_plain_expected = file.read()
 
 with open(expected_dir('tree_expected.txt'), 'r', encoding='utf-8') as file:
     tree_expected = file.read()
 
+with open(expected_dir('tree_plain_expected.txt'), 'r', encoding='utf-8') as file:
+    tree_plain_expected = file.read()
+
+
 flat_fixtures = [
-    (flat_dir('flat_1.json'), flat_dir('flat_2.yaml'), flat_expected),
     (flat_dir('flat_1.yaml'), flat_dir('flat_2.yml'), flat_expected),
+    (flat_dir('flat_1.json'), flat_dir('flat_2.json'), flat_expected),
+]
+flat_plain_fixtures = [
+    (flat_dir('flat_1.yaml'), flat_dir('flat_2.yml'), flat_plain_expected),
+    (flat_dir('flat_1.json'), flat_dir('flat_2.json'), flat_plain_expected),
 ]
 
 tree_fixtures = [
-    (tree_dir('tree_1.json'), tree_dir('tree_2.yaml'), tree_expected),
+    (tree_dir('tree_1.json'), tree_dir('tree_2.json'), tree_expected),
     (tree_dir('tree_1.yml'), tree_dir('tree_2.yaml'), tree_expected),
+]
+tree_plain_fixtures = [
+    (tree_dir('tree_1.json'), tree_dir('tree_2.json'), tree_plain_expected),
+    (tree_dir('tree_1.yml'), tree_dir('tree_2.yaml'), tree_plain_expected),
 ]
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,5 @@
 from gendiff.scripts.generate_diff import generate_diff
-from tests.conftest import tree_fixtures, flat_fixtures
+from tests.conftest import tree_fixtures, flat_fixtures, tree_plain_fixtures, flat_plain_fixtures
 import pytest
 
 
@@ -11,3 +11,13 @@ def test_generate_diff_flat_files(file1, file2, expected):
 @pytest.mark.parametrize("file1, file2, expected", tree_fixtures)
 def test_generate_diff_tree_files(file1, file2, expected):
     assert generate_diff(file1, file2) == expected
+
+
+@pytest.mark.parametrize("file1, file2, expected", flat_plain_fixtures)
+def test_generate_diff_flat_files_in_plain(file1, file2, expected):
+    assert generate_diff(file1, file2, 'plain') == expected
+
+
+@pytest.mark.parametrize("file1, file2, expected", tree_plain_fixtures)
+def test_generate_diff_tree_files_in_plain(file1, file2, expected):
+    assert generate_diff(file1, file2, 'plain') == expected
