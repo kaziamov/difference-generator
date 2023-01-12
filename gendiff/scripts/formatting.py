28c58640f02adcb1de11528e4c79386088337fa8
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2023-01-12 20:21:33 +0300

    Refactor code

--- gendiff/scripts/formatting.py
+++ gendiff/scripts/formatting.py
@@ -1,23 +1,31 @@
 import json
 
 
-def _convert_to_string(data):
+def _convert_to_string(data, indent):
     if type(data) is bool or data is None:
         return json.dumps(data)
     elif type(data) is dict:
-        return map(lambda x: f'{x}')
+        formatted_dict = []
+        for x, y in data.items():
+            key = x
+            value = y
+            if type(y) is dict:
+                value = _convert_to_string(y, f'    {indent}')
+            formatted_line = f'{indent}    {key}: {value}'
+            formatted_dict.append(formatted_line)
+        return '{\n' + '\n'.join(formatted_dict) + '\n' + indent + '}'
     else:
         return data
 
 
-def _get_intend(level=0, spaces_count=4):
+def _get_indent(level=0, spaces_count=4):
     if not level:
         return ''
     return ' ' * spaces_count * level
 
 
-def _get_brakes(data):
-    return '{\n' + data + '\n}'
+def _get_brakes(data, indent):
+    return '{\n' + data + '\n' + indent + '}'
 
 
 def formatting_tree(tree):
@@ -26,57 +34,40 @@ def formatting_tree(tree):
 
 
 def _format_node_(node, level=0):
-    # print(f'node is {node}')
-    intend = _get_intend(level=level)
-    # print(f'intend is "{intend}", and level is {level}')
+    indent = _get_indent(level=level)
 
     key = node.get('key')
-    # print(f'key is {key}')
     status = node.get('status')
-    # print(f'status is {status}')
     childs = node.get('child')
-    # print(f'child is {childs}')
 
-    value = _convert_to_string(node.get('value'))
-    # print(f'value is {value}')
-    value1 = _convert_to_string(node.get('value1'))
-    # print(f'value1 is {value1}')
-    value2 = _convert_to_string(node.get('value2'))
-    # print(f'value2 is {value2}')
+    value = _convert_to_string(node.get('value'), indent=_get_indent(level+1))
+    value2 = _convert_to_string(node.get('value2'), indent=_get_indent(level+1))
 
     if status == 'root':
-        print('root started')
         strings = [_format_node_(child, level=0) for child in childs]
-        print(f'root strings is {strings}')
         result = _get_brakes(
-            '\n'.join(strings)
+            '\n'.join(strings), f'{indent}'
         )
-        # print(f'root result is {result}')
 
     if status == 'added':
-        result = f'{intend}  + {key}: {value}'
-        # print(f'added result is {result}')
+        result = f'{indent}  + {key}: {value}'
 
     if node['status'] == 'removed':
-        result = f'{intend}  - {key}: {value}'
-        # print(f'removed result is {result}')
+        result = f'{indent}  - {key}: {value}'
 
     if status == 'updated':
-        strings = [f'{intend}  - {key}: {value1}',
-                   f'{intend}  + {key}: {value2}']
+        strings = [f'{indent}  - {key}: {value}',
+                   f'{indent}  + {key}: {value2}']
         result = '\n'.join(strings)
-        # print(f'updated result is {result}')
 
     if status == 'same':
-        result = f'{intend}    {key}: {value}'
-        # print(f'same result is {result}')
+        result = f'{indent}    {key}: {value}'
 
     if status == 'child':
         strings = [_format_node_(child, level + 1) for child in childs]
-        # print(f'child strings is {strings}')
-        result = '\n'.join(strings)
-        # print(f'child result is {result}')
+        value = _get_brakes(
+            '\n'.join(strings), f'    {indent}'
+        )
+        result = f'{indent}    {key}: {value}'
 
-    # print(f'result is {result}')
-    # print('______________________')
     return result
--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -44,24 +44,41 @@ def open_two_files(path_to_file1, path_to_file2):
 
 
 def _create_root(data1, data2):
+    """Make differences for root segment from two trees"""
     return {'status': 'root', 'child': make_diff(data1, data2)}
 
 
 def make_diff(data1, data2):
+    """Make structure of differences for childs from two trees"""
     keys = sorted(data1.keys() | data2.keys())
     result = []
     for key in keys:
         status = {'key': key}
         if key not in data2:
-            status.update({'status': 'removed', 'value': data1[key]})
+            status.update(
+                {'status': 'removed',
+                 'value': data1[key]})
+
         elif key not in data1:
-            status.update({'status': 'added', 'value': data2[key]})
+            status.update(
+                {'status': 'added',
+                 'value': data2[key]})
+
         elif type(data1[key]) == dict and type(data2[key]) == dict:
             childs_diff = make_diff(data1[key], data2[key])
-            status.update({'status': 'child', 'child': childs_diff})
+            status.update(
+                {'status': 'child',
+                 'child': childs_diff})
+
         elif data1[key] == data2[key]:
-            status.update({'status': 'same', 'value': data1[key]})
+            status.update(
+                {'status': 'same',
+                 'value': data1[key]})
+
         else:
-            status.update({'status': 'updated', 'value1': data1[key], 'value2': data2[key]})
+            status.update({'status': 'updated',
+                           'value': data1[key],
+                           'value2': data2[key]})
+
         result.append(status)
     return result
