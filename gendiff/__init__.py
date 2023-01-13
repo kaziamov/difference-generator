e644ddf880762bd5e3f908d40da63aa9d3aef38a
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2023-01-13 15:01:11 +0300

    Fix tests errors

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,5 +1,5 @@
 from gendiff.scripts import gendiff
 
 
-def generate_diff(file1, file2):
-    return gendiff.generate_diff(file1, file2)
+def generate_diff(file1, file2, format="stylish"):
+    return gendiff.generate_diff(file1, file2, format)
